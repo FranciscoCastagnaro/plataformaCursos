c64c75619a6e6b887ab9e62ce9b24adf907ad647
FranciscoCastagnaro <francastagnaro@hotmail.com>
2024-08-25 18:23:40 -0300

    adding spring boot proyect

---
+++ src/test/java/com/uade/tpo/plataformaCursos/PlataformaCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.plataformaCursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
